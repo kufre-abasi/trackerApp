ee95d40f6b68078294414976d6909686cd9d465d
Kufre-abasi <kufreabasibassey3@gmail.com>
2022-03-22 14:46:03 +0100

    Admin dashboard created

--- src/main.js
+++ src/main.js
@@ -4,12 +4,28 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import * as firebase from 'firebase/app'
+
+
 Vue.config.productionTip = false
+
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  YOUR_CONFIGURATIONP_CODE_HERE
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,85 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import firebase from 'firebase'
+
+import Driver from '@/pages/Driver'
+import Signin from '@/pages/Signin'
+import SignUp from '@/pages/SignUp'
+import Admin from '@/pages/Admin'
+
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+let router = new Router({
+  routes: [{
+      path: '/driver',
+      name: 'Driver',
+      component: Driver,
+      meta: {
+        requiresAuth: true
+      }
+    },
+
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/signin',
+      name: 'Signin',
+      component: Signin,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+
+  firebase.auth().onAuthStateChanged(userAuth => {
+    if (!userAuth && to.matched.some(record => record.meta.requiresAuth)) {
+      next({
+        name: 'Signin'
+      })
+    } else if (userAuth) {
+      if (to.matched.some(record => record.meta.guest)) {
+        next(from.fullPath)
+      } else {
+        firebase.firestore().collection("roles").doc(userAuth.uid).get().then(snapShot => {
+          if (snapShot.data().isAdmin) {
+            next({
+              name: 'Admin'
+            })
+          } else {
+            next({
+              name: 'Driver'
+            })
+          }
+        })
+      }
+
+    } else {
+      next()
+    }
+  })
+
+  next()
+
+})
+
+
+export default router
